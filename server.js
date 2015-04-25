2283e3bfbc4fa2e5afed293a981df7ea522fa6cf
Mike Guerrette <mk.guerrette@gmail.com>
2015-04-25 19:15:57 -0400

    Using ENV PORT for heroku

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ var PORT = 8080;
 
 // App
 var app = express();
+
+app.set('port', (process.env.PORT || 8080))
 app.get('/', function (req, res) {
   res.send('Hello world!!! Woo!');
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.listen(app.get('port'), function() {
+	console.log('Running on http://localhost:' + app.get('port'));
+});
+
